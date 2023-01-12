44ea989f6b82c766c698067cd03a39289dad8062
Alexander Lind <alex.anonym@gmail.com>
2023-01-12 17:46:13 +0100

    minor build fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+// import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
